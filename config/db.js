0be7ba0eb780332c9988cd6efd5e3f01c24a07c9
Abuzar Asif <abuzarasif10@gmail.com>
2025-07-21 19:22:47 +0500

    Added config file

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
   logging: false,
   pool: {
-    max: 50, // Increase as needed for your DB server
+    max: 80, // Leave 20 for Postgres/system
     min: 0,
     acquire: 30000,
     idle: 10000
